26c8707db33a5079759b0dc4935de69a8fa15a08
Trevor Mwangi <trvrmwangi@gmail.com>
2024-08-14 12:14:11 +0300

    create variables.py and learn

---
+++ Day 2/variables.py
@@ -0,0 +1,35 @@
+# Variables in Python
+'''
+first_name = 'Trevor'
+last_name = 'Mwangi'
+city = 'Nairobi'
+age = 25
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Trevor',
+   'lastname':'Mwangi',
+   'city':'Nairobi'
+   }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+'''
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
