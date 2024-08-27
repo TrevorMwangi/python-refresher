6aad549dc7536d089f020e80b466d2d5ead06df3
Trevor Mwangi <trvrmwangi@gmail.com>
2024-08-27 18:29:35 +0300

    day 2 exercises

---
+++ Day 2/circle.py
@@ -0,0 +1,15 @@
+''' import math
+
+pi = 3.14
+r = 30
+area_of_circle = pi * r * r
+
+print(area_of_circle)
+
+'''
+
+pi = 3.14
+r = 30
+
+circumference = 2 * pi * r
+print(circumference)
---
+++ Day 2/compare.py
@@ -0,0 +1,5 @@
+fname = input("Enter your name: ")
+lname = input("Enter your last name: ")
+
+print(("Length of first name is"),len(fname))
+print(("Length of last name is"),len(lname))
