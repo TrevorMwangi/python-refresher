0662b4561ff4bcf9887f4119647ef29161f244f2
Trevor Mwangi <trvrmwangi@gmail.com>
2024-09-03 18:13:27 +0300

    lambda square list

--- Day 4/lambdaFunction.py
+++ Day 4/lambdaFunction.py
@@ -9,3 +9,11 @@ expression is the single expression that the function evaluates and returns
 
 add = lambda x,y : x + y
 print(add(2,4))
+
+
+# List of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Square each number
+squared_numbers = list(map(lambda x: x ** 2, numbers))
+print(squared_numbers)  # Output: [1, 4, 9, 16, 25]
