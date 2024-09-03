ed7c1d1235f246366f535f907ee81e2ddc5d2d67
Trevor Mwangi <trvrmwangi@gmail.com>
2024-09-03 12:34:00 +0300

    lambda lesson

---
+++ Day 4/lambdaFunction.py
@@ -0,0 +1,11 @@
+'''
+Lambda is a keyword used to create small anonymous functions
+ie. functions without a name
+SYNTAX : lambda arguments : expression
+
+arguments are the parameters the function takes
+expression is the single expression that the function evaluates and returns
+'''
+
+add = lambda x,y : x + y
+print(add(2,4))
